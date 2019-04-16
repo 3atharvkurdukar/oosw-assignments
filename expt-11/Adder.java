5865569cd2f367276a65a1699b609186c4233235
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2019-04-16 16:18:16 +0530

    Added experiment 11 with output

---
+++ expt-11/Adder.java
@@ -0,0 +1,7 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Adder extends Remote {
+
+    public int add(int x, int y) throws RemoteException;
+}
---
+++ expt-11/MyServer.java
@@ -0,0 +1,13 @@
+import java.rmi.Naming;
+
+public class MyServer {
+
+    public static void main(String[] args) {
+        try {
+            Adder stub = new AdderRemote();
+            Naming.rebind("rmi://localhost:5000/myserver", stub);    
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
